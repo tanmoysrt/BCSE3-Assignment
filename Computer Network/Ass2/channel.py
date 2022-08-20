73e3904ce845637f1fe536781db0c49bb5f4e6a1
Tanmoy Sarkar <ts741127@gmail.com>
2022-08-21 03:18:19 +0530

    update

--- Computer Network/Ass2/channel.py
+++ Computer Network/Ass2/channel.py
@@ -1,5 +1,5 @@
 from socketServer import SocketServer
-
+import random
 
 class Channel(SocketServer):
     def __init__(self, host, port):
@@ -12,12 +12,17 @@ class Channel(SocketServer):
         print(data)
         return data
     
-    
+    @staticmethod
+    def injectErrorInData(data, loopC=5):
+        for _ in range(loopC):
+            random_bit_location = random.randint(0, len(data)-1)
+            data = data[:random_bit_location] +  ['0','1'][random.randint(0,1)] + data[random_bit_location+1:]
+        return data
 
 
 
 
-tmp = Channel(host='127.0.0.1', port=8081)
-tmp.start()
+# tmp = Channel(host='127.0.0.1', port=8081)
+# tmp.start()
 # tmp.startAcceptConnections()
-SocketServer.modifyData('Hello')
+# SocketServer.modifyData('Hello')
